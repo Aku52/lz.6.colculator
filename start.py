8aefdf6ae88c635ac52473aa85e1e1d1312cecc9
Aku52 <kotik.52@tutamail.com>
2024-11-29 13:30:07 +0300

    colculator.2

--- start.py
+++ start.py
@@ -1,28 +1,69 @@
 print('Введите выражение')
-user_unput=input()
+user_input=input()
 
-def addition (a,b):
+def add (a,b):
     return a + b
 
-def difference (a,b):
+def subtrac (a,b):
     return a - b
 
-def multiplication(a,b):
+def multiply(a,b):
     return a * b
 
-def division  (a,b):
+def divide (a,b):
     return a / b
 
 
+# split - разделяет вфражение
+if '+' in user_input:
+    result = 0
+    user_input = user_input.split('+') 
+    for i in range (0,len(user_input)):
+        user_input[i]= float(user_input[i])
+        result= add(result,user_input[i])
+    print(result)
+    result = 0
 
-if '+' in user_unput:
-    user_unput = user_unput.split('+')
-    for i in range (0,len(user_unput)):
-        user_unput[i]= float(user_unput[i])
-        result= 
-        
-#addition (сложение)
-#subtraction (вычитание) difference(разность)
-#multiplication(умножить)
-#division (делить)
-#user (пользователь)
+
+elif '-' in user_input:
+    result = 0
+    user_input = user_input.split('-')
+    for i in range (0,len(user_input)):
+        user_input[i]= float(user_input[i])
+        result= subtrac(result,user_input[i])
+    print(result)
+
+
+elif '*' in user_input:
+    result = 1
+    user_input = user_input.split('*')
+    for i in range (0,len(user_input)):
+        user_input[i]= float(user_input[i])
+        result= multiply(result,user_input[i])
+    print(result)
+
+    
+elif '/' in user_input:
+    result = 1
+    user_input = user_input.split('*')
+    for i in range (0,len(user_input)):
+        user_input[i]= float(user_input[i])
+        result= divide(result,user_input[i])
+    print(result)
+
+
+'''quan_list = [] # список для элементов
+
+quan = 0
+for i in range (0,len(user_input)):
+    quan += user_input[i]
+    quan_list.append(quan)'''
+
+
+
+
+# add(сложить)
+# subtrac (вычитать)
+# multiply(умножить)
+# divide(делить)
+#user (пользователь), quantity - количество
