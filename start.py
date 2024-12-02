0145328fbe23c39f40ecfc45760a4e44183919ff
Aku52 <kotik.52@tutamail.com>
2024-12-02 17:29:18 +0300

    colculator.3

--- start.py
+++ start.py
@@ -16,8 +16,8 @@ def divide (a,b):
 
 # split - разделяет вфражение
 if '+' in user_input:
-    result = 0
-    user_input = user_input.split('+') 
+    user_input = user_input.split('+')
+    result = float(user_input[0])
     for i in range (0,len(user_input)):
         user_input[i]= float(user_input[i])
         result= add(result,user_input[i])
@@ -26,39 +26,33 @@ if '+' in user_input:
 
 
 elif '-' in user_input:
-    result = 0
     user_input = user_input.split('-')
-    for i in range (0,len(user_input)):
+    result = float(user_input[0])
+    for i in range (1,len(user_input)):
         user_input[i]= float(user_input[i])
         result= subtrac(result,user_input[i])
     print(result)
+    result = 0
 
 
 elif '*' in user_input:
-    result = 1
     user_input = user_input.split('*')
-    for i in range (0,len(user_input)):
+    result =  float(user_input[0])
+    for i in range (1,len(user_input)):
         user_input[i]= float(user_input[i])
         result= multiply(result,user_input[i])
     print(result)
+    result = 0
 
     
 elif '/' in user_input:
-    result = 1
     user_input = user_input.split('*')
-    for i in range (0,len(user_input)):
+    result =  float(user_input[0])
+    for i in range (1,len(user_input)):
         user_input[i]= float(user_input[i])
         result= divide(result,user_input[i])
     print(result)
-
-
-'''quan_list = [] # список для элементов
-
-quan = 0
-for i in range (0,len(user_input)):
-    quan += user_input[i]
-    quan_list.append(quan)'''
-
+    result = 0
 
 
 
